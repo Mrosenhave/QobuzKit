46113864547bd2aa00b2e204e4f17dd90c53973a
Mrosenhave <mrosenhave@gmail.com>
2025-02-19 01:08:53 +0100

    fix

--- Sources/QobuzKit/QobuzKit.swift
+++ Sources/QobuzKit/QobuzKit.swift
@@ -5,6 +5,30 @@ import Foundation
 
 public struct QobuzAPI {}
 
-public struct QobuzClient {
-  public init() {}
+public class QobuzClient:ObservableObject {
+  @MainActor static let shared = QobuzClient (appId: "", appSecret: "")
+  public init(appId: String, appSecret: String) {
+    self.appId = appId
+    self.appSecret = appSecret
+  }
+  
+  @Published public var appId: String
+  @Published public var appSecret: String
+  
+  @MainActor public func generateCredentials() async {
+    let QBC = QobuzCredential()
+    guard let bundle = await QBC.getBundle() else {return}
+    guard let appId = QBC.getAppId(bundle), let appSecret = QBC.getSecrets(bundle)?.last else {return}
+    self.setCredentials(appId: appId, appSecret: appSecret)
+  }
+  
+  @MainActor
+  public func setCredentials(appId: String, appSecret: String) {
+    self.appId = appId
+    self.appSecret = appSecret
+  }
+}
+
+public struct QobuzCredential {
+  
 }
