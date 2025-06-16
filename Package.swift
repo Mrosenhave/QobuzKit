fa644211c44f26d2a0ba26357ac9433cc4f36b61
Mrosenhave <mrosenhave@gmail.com>
2025-06-17 00:44:22 +0200

    Added more supported platforms

--- Package.swift
+++ Package.swift
@@ -6,7 +6,11 @@ import PackageDescription
 let package = Package(
     name: "QobuzKit",
     platforms: [
-      .iOS(.v16)
+      .iOS(.v16),
+      .macOS(.v13),
+      .watchOS(.v9),
+      .tvOS(.v16),
+      .visionOS(.v1)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
